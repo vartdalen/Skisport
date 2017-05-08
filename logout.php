aa7100609be0dad45b836870f7ad21062369211d
Vartdalen <s315615@stud.hioa.no>
2017-05-08 14:23:27 +0200

    liten tiss

--- logout.php
+++ logout.php
@@ -32,6 +32,62 @@
     
     ?>
       
+    <nav class="navbar navbar-inverse navbar-fixed-top">
+      <div class="container" id="c1">
+          <div class="navbar-header">
+              <a class="navbar-brand " href="forside.php">Hjem</a>
+          </div>
+          <div id="navbar" class="collapse navbar-collapse">
+              <ul class="nav navbar-nav">
+                  <li><a href="form.php">Arrangementer</a></li>
+              </ul>
+
+              <div class="dropdown">
+
+                  <ul class="nav navbar-nav" style="float:right;">
+
+                      <li role="presentation" class="dropdown" >
+                          <a class="dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">
+                            Bruker <span class="caret"></span>
+                          </a>
+                          <ul class="dropdown-menu">
+                              <?php
+
+                                  if(!isset($_SESSION['user'])) { 
+
+                                  echo "<li><a href='registrerBruker.php'>Registrer deg</a></li>";
+
+                                  }
+
+                              ?>
+
+                              <li><a href="oppdaterInfo.php">Oppdater informasjon</a></li>
+                              <li><a href="admin.php">Admin</a></li>
+                              <li role="separator" class="divider"></li>
+
+                              <?php
+
+                                  if(isset($_SESSION['user'])) {
+
+                                  echo "<li><a href='logout.php'>Logg ut</a></li>";
+
+                                  } else {
+
+                                  echo "<li><a href='loginPage.php'>Logg inn</a></li>";
+
+                                  }
+
+                              ?>
+                          </ul>
+                      </li>
+                  </ul>
+
+              </div>
+
+      </div>
+  </nav>
+ 
+      
     <!-- JQuery -->
     <script src="js/jquery.min.js"></script>
     
